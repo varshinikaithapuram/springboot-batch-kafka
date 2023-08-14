deb201697faf50030166b519256cd8a4f8bc7f9a
varshinikaithapuram <varshinikaithapuram@gmail.com>
2023-08-14 16:45:05 -0500

    spring boot scheduler project

---
+++ spring-boot-scheduler-example/src/test/java/com/example/schedulerdemo/SchedulerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SchedulerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
